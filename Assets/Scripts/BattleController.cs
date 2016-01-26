6fcfd1fac3a23db03387023e32628a08f5200ffd
Joshua Rand <JRand@student.dwc.edu>
2016-01-26 11:28:54 -0500

    Added a box to the canvas

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -10,17 +10,20 @@ public class BattleController : MonoBehaviour {
 	void Start () {
         StartCoroutine(Run());
         MagicCircleMaterial = MagicCircle.GetComponent<MeshRenderer>().material;
-        //MagicCircleMaterial.SetColor("_TintColor", new Color(0f, 0f, 1f, 1f));
         StartCoroutine(SpinCircle());
 	}
 
     IEnumerator SpinCircle()
     {
         float angle = 0f;
+        float alphaCount = 0f;
         while (true)
         {
             angle += 3f;
+            alphaCount += 0.1f;
+            float alpha = 0.4f + 0.2f * Mathf.Cos(alphaCount);
             MagicCircle.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            MagicCircleMaterial.SetColor("_TintColor", new Color(1f, 1f, 1f, alpha));
             yield return 0;
         }
     }
