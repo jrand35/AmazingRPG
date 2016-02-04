02d13e29ce8d73a7a0fc4504975ec1c6d39fbf8c
Joshua Rand <JRand@student.dwc.edu>
2016-02-03 23:01:16 -0500

    ...

--- Assets/Scripts/RatBehavior.cs
+++ Assets/Scripts/RatBehavior.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 
 public class RatBehavior : BattleBehavior
 {
+    private Animator anim;
     public RatBehavior(Battler parent)
     {
         Battler = parent;
+        anim = parent.gameObject.GetComponent<Animator>();
     }
 
     public override void Initialize()
@@ -37,8 +39,10 @@ public class RatBehavior : BattleBehavior
 
     public override IEnumerator StandardAttack(Battler user, Battler target)
     {
+        anim.SetInteger("State", 1);
         Vector3 startPos = user.gameObject.transform.position;
         yield return Move.MoveInFrontOfBattler(user, Target, startPos, new Vector3(2f, 0f, 0f));
+        anim.SetInteger("State", 0);
         int baseDamage = (user.BattleBehavior.Stats.Attack * 4) - (Target.BattleBehavior.Stats.Defense * 2);
         if (baseDamage > 0)
             baseDamage = new System.Random().Next((int)(baseDamage * 0.9), (int)(baseDamage * 1.1));
@@ -46,6 +50,8 @@ public class RatBehavior : BattleBehavior
             baseDamage = 0;
         Target.BattleBehavior.TakeDamage(user, baseDamage);
         yield return new WaitForSeconds(0.5f);
+        anim.SetInteger("State", 1);
         yield return Move.MoveBackFromBattler(user, Target, startPos, new Vector3(2f, 0f, 0f));
+        anim.SetInteger("State", 0);
     }
 }
