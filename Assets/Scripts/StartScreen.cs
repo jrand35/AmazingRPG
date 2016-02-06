8a25c98188271aa5f62e1ae2c364e3fbf837c3bc
Joshua Rand <JRand@student.dwc.edu>
2016-02-05 22:22:42 -0500

    Added title screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+    public Text text;
+
+    void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("Scene");
+        }
+    }
+
+    IEnumerator Run()
+    {
+        float count = 0f;
+        Color col = text.color;
+        Vector3 scale = text.transform.localScale;
+        while (true)
+        {
+            count += 0.1f;
+            float color = 0.5f + 0.25f * Mathf.Sin(count);
+            col.b = color;
+            scale.x = 0.2f + 0.01f * Mathf.Sin(count);
+            scale.y = 0.2f + 0.01f * Mathf.Sin(count);
+            text.color = col;
+            text.transform.localScale = scale;
+            yield return 0;
+        }
+    }
+}
